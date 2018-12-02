68b49642ede6658d1afe0cdb342f62efab77e486
Charl <meyers.charl@gmail.com>
2018-12-02 15:32:00 +0200

    Add ability to change selected month

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
-var DrawCalendar = function () {
+function FormatMonthAndYear(momentObj) {
+    return momentObj.format("MMMM") + " - " + momentObj.format("YYYY");
+}
+
+function DrawCalendar(chosenMonth) {
     var tableBody = document.querySelector("#body");
-    document.querySelector("#monthAndYear").innerText = moment().format("MMMM") + " - " + moment().format("YYYY");
-    document.querySelector("title").innerText = "Simple Calandar | " + moment().format("D MMMM YYYY");
+    var currentDate = moment(chosenMonth);
+    document.querySelector("#monthAndYear").innerText = FormatMonthAndYear(currentDate);
+    document.querySelector("title").innerText = "Simple Calandar | " + currentDate.format("D MMMM YYYY");
 
     //Based off a combination of the answers at https://stackoverflow.com/questions/39786372/creating-a-custom-calendar-with-moment-using-days-weeks-and-headings
-    const startDay = moment().clone().startOf('month').startOf('week');
-    const endDay = moment().clone().endOf('month').endOf('week');
-    
+    const startDay = currentDate.clone().startOf('month').startOf('week');
+    const endDay = currentDate.clone().endOf('month').endOf('week');
+
     let calendar = [];
     var index = startDay.clone().subtract(1, 'day');
     while (index.isBefore(endDay, 'day')) {
         var tableRow = document.createElement("tr");
 
         Array(7).fill(0).map(
-            function(n, i) {
+            function (n, i) {
                 return index.add(1, 'day').clone();
             }
-        ).forEach(function(date){
+        ).forEach(function (date) {
             var column = document.createElement("td");
             var span = document.createElement("span");
             var day = date.date();
 
-            if (date.month() !== moment().month()) {
+            if (date.month() !== currentDate.month()) {
                 column.classList.add('faded');
             } else {
                 span.setAttribute("id", day);
@@ -36,12 +41,34 @@ var DrawCalendar = function () {
         tableBody.appendChild(tableRow)
     }
 
-    document.getElementById(moment().date()).classList.add("currentDay");
+    if (moment().month() === currentDate.month()) {
+        document.getElementById(moment().date()).classList.add("currentDay");
+    }
+}
+
+var InitialiseCalendar = function () {
+    DrawCalendar();
+};
+
+function RedrawCalendar(increment) {
+    document.querySelector("#body").innerHTML = "";
+    var currentMonth = document.querySelector("#monthAndYear").innerText;
+    var newMonth = FormatMonthAndYear(moment(currentMonth).add(increment, "month"));
+
+    DrawCalendar(newMonth);
+}
+
+var NextMonth = function () {
+    RedrawCalendar(1);
+}
+
+var PreviousMonth = function () {
+    RedrawCalendar(-1);
 }
 
 if (document.readyState.toLowerCase() === "complete" ||
     (document.readyState.toLowerCase() !== "loading" && !document.documentElement.doScroll)) {
-    DrawCalendar();
+    InitialiseCalendar();
 } else {
-    document.addEventListener("DOMContentLoaded", DrawCalendar);
+    document.addEventListener("DOMContentLoaded", InitialiseCalendar);
 }
