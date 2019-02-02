6e2ebc3d515c8f23c9c28fe7823a0ca8e17d03b8
Charl <meyers.charl@gmail.com>
2019-02-02 19:35:29 +0200

    Added Google Calendar API #1
    - Added code from get started page as a test

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ function DrawCalendar(chosenMonth) {
     while (index.isBefore(endDay, 'day')) {
         var tableRow = document.createElement("tr");
 
-        for(var days = 0; days < 7; days++) {
+        for (var days = 0; days < 7; days++) {
             var date = index.add(1, 'day').clone();
             var column = document.createElement("td");
             var span = document.createElement("span");
@@ -43,8 +43,112 @@ function DrawCalendar(chosenMonth) {
     }
 }
 
+var authorizeButton = function(){
+    return document.getElementById('authorize_button');
+}
+var signoutButton = function(){
+    return document.getElementById('signout_button');
+}
+
+function initClient() {
+    gapi.client.init({
+        apiKey: 'removed-for-security-resons',
+        clientId: 'removed-for-security-resons',
+        discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"],
+        scope: 'https://www.googleapis.com/auth/calendar.readonly'
+    }).then(function () {
+        // Listen for sign-in state changes.
+        gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+
+        // Handle the initial sign-in state.
+        updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+        authorizeButton().onclick = handleAuthClick;
+        signoutButton().onclick = handleSignoutClick;
+    }, function (error) {
+        appendPre(JSON.stringify(error, null, 2));
+    });
+}
+
+/**
+ *  Called when the signed in status changes, to update the UI
+ *  appropriately. After a sign-in, the API is called.
+ */
+function updateSigninStatus(isSignedIn) {
+    if (isSignedIn) {
+        authorizeButton().style.display = 'none';
+        signoutButton().style.display = 'block';
+        listUpcomingEvents();
+    } else {
+        authorizeButton().style.display = 'block';
+        signoutButton().style.display = 'none';
+    }
+}
+
+/**
+ *  Sign in the user upon button click.
+ */
+function handleAuthClick(event) {
+    gapi.auth2.getAuthInstance().signIn();
+}
+
+/**
+ *  Sign out the user upon button click.
+ */
+function handleSignoutClick(event) {
+    gapi.auth2.getAuthInstance().signOut();
+}
+
+/**
+ * Append a pre element to the body containing the given message
+ * as its text node. Used to display the results of the API call.
+ *
+ * @param {string} message Text to be placed in pre element.
+ */
+function appendPre(message) {
+    // var pre = document.getElementById('content');
+    // var textContent = document.createTextNode(message + '\n');
+    // pre.appendChild(textContent);
+    console.log(message);
+}
+
+/**
+ * Print the summary and start datetime/date of the next ten events in
+ * the authorized user's calendar. If no events are found an
+ * appropriate message is printed.
+ */
+function listUpcomingEvents() {
+    gapi.client.calendar.events.list({
+        'calendarId': 'primary',
+        'timeMin': (new Date()).toISOString(),
+        'showDeleted': false,
+        'singleEvents': true,
+        'maxResults': 10,
+        'orderBy': 'startTime'
+    }).then(function (response) {
+        var events = response.result.items;
+        appendPre('Upcoming events:');
+
+        if (events.length > 0) {
+            for (i = 0; i < events.length; i++) {
+                var event = events[i];
+                var when = event.start.dateTime;
+                if (!when) {
+                    when = event.start.date;
+                }
+                appendPre(event.summary + ' (' + when + ')')
+            }
+        } else {
+            appendPre('No upcoming events found.');
+        }
+    });
+}
+
+
 var InitialiseCalendar = function () {
     DrawCalendar();
+    // 1. Load the JavaScript client library.
+    //gapi.load('client', start);
+    gapi.load('client:auth2', initClient);
 };
 
 function RedrawCalendar(increment) {
