ef6f19db9daf933e495079a4d2d968933539b579
Charl <meyers.charl@gmail.com>
2019-04-24 22:04:31 +0200

    Add ability to add events

--- PythonCalendar/main.py
+++ PythonCalendar/main.py
@@ -9,26 +9,63 @@ width = 640
 cal = calendar.Calendar()
 calendarGrid = Image.new('1', (width, height), constants.WHITE)
 
+def get_vertical_step():
+	return math.ceil((width - (constants.CALENDAR_BORDER * 2)) / 7)
+
+
+def get_horizontal_step(number_of_weeks):
+	return math.ceil((height - constants.CALENDAR_TOP - (constants.CALENDAR_BORDER * 2)) / number_of_weeks)
+
+
+def determine_max_sentance_length(character_width, ellipsis_width):
+	cell_width = get_vertical_step() + ellipsis_width
+	answer = math.ceil(cell_width / character_width)
+
+	return answer
+
+
 def underline_text(draw_element, coordinate_x, coordinate_y, text, font):
+	# From https://stackoverflow.com/questions/8868564/draw-underline-text-with-pil
 	text_height, text_width = draw_element.textsize(text, font)
-	padding = 2
+	padding = constants.LINE_PADDING
 	draw_element.text((coordinate_x, coordinate_y), text, constants.FONT_COLOR, font)
 	draw_element.line((coordinate_x, coordinate_y + text_height + padding, coordinate_x + text_width, coordinate_y + text_height + padding))
 
-def write_calendar_day(draw_element, coordinate_x, coordinate_y, calendar_day):
+
+def add_event(draw_element, coordinate_x, coordinate_y, events):
+	text_height, text_width = draw_element.textsize('A', constants.DATE_FONT) # Trying to use the widest character
+	ellipsis_height, ellipsis_width = draw_element.textsize(constants.ELLIPSIS, constants.DATE_FONT)
+	max_event_length = determine_max_sentance_length(text_width, ellipsis_width)
+
+	print(max_event_length)
+
+	text_height += constants.CELL_PADDING
+
+	for event in events:
+		if len(event) > max_event_length:
+			event = event[:max_event_length] + constants.ELLIPSIS
+
+		draw_element.text((coordinate_x, coordinate_y + text_height), event, constants.FONT_COLOR, constants.DATE_FONT)
+		coordinate_y += text_height
+
+
+def write_calendar_day(draw_element, coordinate_x, coordinate_y, calendar_day, events):
 	if calendar_day == constants.TODAY.day: #TODO rather encircle
 		underline_text(draw_element, coordinate_x, coordinate_y, str(calendar_day), constants.DATE_FONT)
 	else:
 		draw_element.text((coordinate_x, coordinate_y),
 					  str(calendar_day), constants.FONT_COLOR, constants.DATE_FONT)
 
+	if calendar_day in events:
+		add_event(draw_element, coordinate_x, coordinate_y, events[calendar_day])
 
-def build_calendar():
+
+def build_calendar(events):
 	calendar_end_coordinates_width = width - constants.CALENDAR_BORDER
 	calendar_end_coordinates_height = height - constants.CALENDAR_BORDER
 	number_of_weeks = len(calendar.monthcalendar(constants.TODAY.year, constants.TODAY.month))
-	vertical_step = math.ceil((width - (constants.CALENDAR_BORDER * 2)) / 7)
-	horizontal_step = math.ceil((height - constants.CALENDAR_TOP - (constants.CALENDAR_BORDER * 2)) / number_of_weeks)
+	vertical_step = get_vertical_step()
+	horizontal_step = get_horizontal_step(number_of_weeks)
 	calendar_header_offset = constants.WEEK_HEADER_HEIGHT + constants.CALENDAR_HEADER_HEIGHT
 	calendar_day_name_offset = constants.WEEK_HEADER_HEIGHT + constants.CALENDAR_TOP
 
@@ -65,16 +102,16 @@ def build_calendar():
 		if date != 0:
 			if coordinates_vertical != constants.CALENDAR_BORDER and coordinates_horizontal != 0:
 				write_calendar_day(draw, coordinates_vertical + constants.CELL_PADDING,
-								   coordinates_horizontal + calendar_header_offset, date)
+								   coordinates_horizontal + calendar_header_offset, date, events)
 			elif coordinates_vertical != constants.CALENDAR_BORDER:
 				write_calendar_day(draw, coordinates_vertical + constants.CELL_PADDING,
-								   coordinates_horizontal + calendar_day_name_offset, date)
+								   coordinates_horizontal + calendar_day_name_offset, date, events)
 			elif coordinates_horizontal == 0 and coordinates_vertical == constants.CALENDAR_BORDER:
 				write_calendar_day(draw, constants.CALENDAR_BORDER + constants.CELL_PADDING,
-								   coordinates_horizontal + calendar_day_name_offset, date)
+								   coordinates_horizontal + calendar_day_name_offset, date, events)
 			else:
 				write_calendar_day(draw, constants.CALENDAR_BORDER + constants.CELL_PADDING,
-								   coordinates_horizontal + calendar_header_offset, date)
+								   coordinates_horizontal + calendar_header_offset, date, events)
 
 		coordinates_vertical += vertical_step
 		if day_in_month % 7 == 0:
@@ -84,5 +121,12 @@ def build_calendar():
 		day_in_month += 1
 
 
-build_calendar()
+def generate_events():
+	events = {2: ["Public Holiday"], 6: ["Random reminder"],
+			  15: ["A very long reminder", "Hare sny", "A reminder that is supposed to overflow"]}
+
+	return events
+
+
+build_calendar(generate_events())
 calendarGrid.show()
