611a58641a8e9caf9de95c953d0e92723aaf34ff
Charl <meyers.charl@gmail.com>
2019-04-28 16:01:23 +0200

    Introduce an interface for the screen

--- PythonCalendar/main.py
+++ PythonCalendar/main.py
@@ -1,6 +1,12 @@
 import googleCalendarWorker
 import calendarBuilder
+import screenInterface
 
 events = googleCalendarWorker.get_events()
-calendarGrid = calendarBuilder.build_calendar(events)
-calendarGrid.show()
+screen_width, screen_height = screenInterface.get_screen_resolution()
+
+builder = calendarBuilder.CalendarBuilder(screen_width, screen_height)
+calendarGrid = builder.build_calendar(events)
+
+screenInterface.send_to_screen(calendarGrid)
+
---
+++ PythonCalendar/screenInterface.py
@@ -0,0 +1,28 @@
+import epd7in5
+import constants
+from PIL import ImageFont,ImageDraw, Image
+
+screen = epd7in5.EPD()
+screen.init()
+
+
+def display_text(text):
+	text_image = Image.new('1', (epd7in5.EPD_WIDTH, epd7in5.EPD_HEIGHT), constants.WHITE)
+	text_image_draw = ImageDraw.Draw(text_image)
+	text_display_font = ImageFont.truetype(constants.FONT_REGULAR, 24)
+	text_image_draw.text((0,0), text, constants.FONT_COLOR, text_display_font)
+	send_to_screen(text_image)
+
+
+def send_to_screen(image):
+	screen.display(screen.getbuffer(image))
+	screen.sleep()
+
+
+def clear_screen():
+	screen.Clear(0xFF)
+
+
+def get_screen_resolution():
+	return epd7in5.EPD_WIDTH, epd7in5.EPD_HEIGHT
+
