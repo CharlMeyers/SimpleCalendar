70de5022cbfcd3454abacaf80973021676d9f061
Charl <meyers.charl@gmail.com>
2020-02-29 12:42:46 +0200

    Updates to when screen is cleared
    - Changed screen clearing to only happen on Sundays or the first day
    of the month

--- src/main.py
+++ src/main.py
@@ -5,7 +5,10 @@ from utils import calendarBuilder
 
 from utils import screenInterface
 
+import constants
+
 calendar_ids = []
+
 with open(os.path.join(os.path.dirname(__file__), 'calendars.id'), 'r') as calendar_ids_file:
 	for id in calendar_ids_file:
 		if id[0] != '#':
@@ -20,8 +23,12 @@ print("Building calendar")
 builder = calendarBuilder.CalendarBuilder(screen_width, screen_height)
 calendarGrid = builder.build_calendar(events)
 
-print("Clearing screen")
-screenInterface.clear_screen()
+# If it Sunday or the first day of the month clear out the screen
+# This is because it is recommended to clear the screen at least once every 10 days
+# and the calendar grid will be rebuilt on a new month anyway
+if constants.TODAY.isoweekday() == 7 or constants.TODAY.day == 1:
+	print("Clearing screen")
+	screenInterface.clear_screen()
 
 print("Displaying calendar")
 screenInterface.send_to_screen(calendarGrid)
