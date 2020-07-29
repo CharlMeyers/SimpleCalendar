d029801f5652e83a8b592ae3fd585dafe2b79a3f
Charl <meyers.charl@gmail.com>
2020-07-29 19:45:40 +0200

    Update screenInterface
    - Reduce font size for messages

--- src/utils/screenInterface.py
+++ src/utils/screenInterface.py
@@ -9,7 +9,7 @@ screen.init()
 def display_text(text):
 	text_image = Image.new('1', (epd7in5.EPD_WIDTH, epd7in5.EPD_HEIGHT), constants.WHITE)
 	text_image_draw = ImageDraw.Draw(text_image)
-	text_display_font = ImageFont.truetype(constants.FONT_REGULAR, 24)
+	text_display_font = ImageFont.truetype(constants.FONT_REGULAR, 14)
 	text_image_draw.text((0,0), text, constants.FONT_COLOR, text_display_font)
 	send_to_screen(text_image)
 
